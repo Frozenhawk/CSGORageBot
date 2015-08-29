1d75aa52187238705212a28cd4d5153151d5fdf2
Scott-Plummer <Scott.Plummer18@gmail.com>
2015-08-28 22:10:30 -0700

    Added page scrolling and fixed JS popup interaction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,30 +10,46 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
-    public static void main(String args[]) {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://csgorage.com/raffles/current");
+    public static void main(String args[]) throws InterruptedException {
+        ProfilesIni profile = new ProfilesIni();
+        FirefoxProfile ffprofile = profile.getProfile("default");
+        WebDriver driver = new FirefoxDriver(ffprofile);
+        driver.get("http://csgorage.com/free-raffles/current");
         List<WebElement> raffleDivs = driver.findElements(By.cssSelector("" +
                 ".raffle_box_lg"));
         for (int i = 0; i < raffleDivs.size(); i++) {
             raffleDivs.get(i).findElement(By.cssSelector("a")).click();
-            //itemEntry(driver);
-            driver.navigate().back();
-            raffleDivs = driver.findElements(By.cssSelector("" +
-                    ".raffle_box_lg"));
+            int timer = 17;
+            int currentTime = 0;
+            JavascriptExecutor js = (JavascriptExecutor)driver;
+            do{
+                js.executeScript("window.scrollBy(0,250)", "");
+                Thread.sleep(5000);
+                currentTime++;
+            }while(currentTime < timer);
+            //TODO scrolling to bottom of the page
+            itemEntry(driver);
+            //driver.navigate().back();
+            //raffleDivs = driver.findElements(By.cssSelector("" +
+                   // ".raffle_box_lg"));
         }
 
     }
     public static void itemEntry(WebDriver d) {
-        WebElement slot = d.findElement(By.className("slots"));
-        //slot.click();
+        WebElement slot = d.findElement(By.xpath("//div[@class='slots']"));
+        if(slot == null) return;
+        System.out.println(slot.getTagName());
+        slot.click();
+        d.findElement(By.xpath("//button[@id='getslots']")).click();
         try {
             Alert alert = d.switchTo().alert();
-            if (!alert.getText().equals("x")) {
+            if (!alert.getText().equals("Success!")) {
                 alert.accept();
                 itemEntry(d);
             }
